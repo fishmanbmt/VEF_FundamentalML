0376a796e8bbb14d1d390e2200a684dd3ed19d63
fishman <fishmanbmt@gmail.com>
2020-06-28 22:17:15 +0700

    problem4: count the number of vowels

---
+++ week_1/problem_4/problem3.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python3
+
+
+def countVowel(s):
+	vowels = ['u', 'e', 'o', 'a', 'i']
+	ret = 0
+	for i in s:
+		if i in vowels:
+			ret += 1
+
+	return ret
+
+
+msg = input('Enter a String: ')
+print('The number of vowels : {}'.format(countVowel(msg)))
